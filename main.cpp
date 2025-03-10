bdc46f1006f867c175f8d124f489ec3546ac65f9
francois gourbal <gourbal.francois@gmail.com>
2025-03-10 14:32:39 +0100

    test

--- main.cpp
+++ main.cpp
@@ -11,3 +11,29 @@ int main(int argc, char *argv[]) {
 
     return app.exec();
 }
+
+//#include <QApplication>
+//#include <QDebug>
+//#include "include/game.h"
+//
+//#include "server/GameServer/gameserver.h"
+//#include "client/GameClient/gameclient.h"
+//
+//int main(int argc, char *argv[]) {
+//    QApplication a(argc, argv);
+//
+//    auto server = new GameServer(&a);
+//    server->startServer(12345, 1, false);
+//
+//    auto client = new GameClient(&a);
+//    // client->connectToServer(QHostAddress("10.31.34.229"), 12345);
+//    client->connectToServer(QHostAddress::LocalHost, 12345);
+//
+//    // QTimer::singleShot(3000, [=](){
+//    //     client->selectRole("p2");
+//    // });
+//
+//    Game game;
+//    game.show();
+//    return a.exec();
+//}
