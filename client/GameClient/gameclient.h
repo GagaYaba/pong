8bcf2d9ad24b2850c179f07ced569a9275027c80
Flimar311 <bentogliomateo@gmail.com>
2025-03-10 09:22:48 +0100

    Add Paddle System Network

--- client/GameClient/gameclient.h
+++ client/GameClient/gameclient.h
@@ -10,6 +10,10 @@
 #include "ClientEventHandler.h"
 #include "ClientEventHandlerFactory.h"
 
+#include <QTimer>
+#include <QDataStream>
+
+
 class GameClient : public QObject
 {
     Q_OBJECT
@@ -28,6 +32,9 @@ public:
 
     // Envoyer la position de la raquette
     void sendPaddlePosition(float paddleY);
+    void sendPaddlePositionBinary(float paddleY);
+    void startPaddleSimulation(); // si vous voulez démarrer explicitement la simulation
+
     int playerId;
 
 signals:
@@ -36,12 +43,16 @@ signals:
 private slots:
     // Gestion des données reçues du serveur
     void onDataReceived();
+    void simulatePaddleData();
 
 
 private:
     QTcpSocket *tcpSocket;
     QHostAddress serverAddress;
-    float lastPaddleY = -1;
+
+    float lastPaddleY { -1.0f }; // valeur initiale
+    QTimer *simulationTimer;
+    float simulationPaddleY { 0.0f };
 
     // Liste des gestionnaires d'événements
     std::vector<std::unique_ptr<ClientEventHandler>> handlers;
